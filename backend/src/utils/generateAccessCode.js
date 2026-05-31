1440d1cb7adfe5ef59bb0541e69fb182227dacca
UUbayS <ubaysam354@gmail.com>
2026-05-31 19:20:35 +0700

    fix: use crypto.randomBytes() for secure access code generation

--- backend/src/utils/generateAccessCode.js
+++ backend/src/utils/generateAccessCode.js
@@ -1,9 +1,12 @@
+const crypto = require('crypto');
+
 function generateAccessCode(length = 6) {
   const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+  const randomBytes = crypto.randomBytes(length);
   let result = '';
 
   for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
+    result += chars.charAt(randomBytes[i] % chars.length);
   }
 
   return result;
